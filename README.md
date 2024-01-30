83cc01fe828662f69f8a6ccf6473dc3a5d87f1e7
Hatice Dilmac <hdilmac@tederga.com>
2024-01-30 13:17:29 +0300

    Add QrCode page

--- README.md
+++ README.md
@@ -1,16 +1,64 @@
-# brightness_settings
+# QR Code Screen Brightness Example
 
-A new Flutter project.
+This Flutter application demonstrates how to generate a QR code and control screen brightness based on navigation events.
 
-## Getting Started
+## Description
 
-This project is a starting point for a Flutter application.
+The app consists of a single page (`RouteAwarePage`) where a QR code is displayed with a URL link. The QR code leads to a shortened URL directing to a specific destination. As soon as this page is opened, the screen brightness automatically increases, thus solving the problem of not being able to read the code due to brightness during turnstile passages.
 
-A few resources to get you started if this is your first Flutter project:
+When navigating to and from the `RouteAwarePage`, the screen brightness is adjusted accordingly:
+- When the page is pushed, the brightness is set to 70%.
+- When navigating away from the page, the brightness is reset to its default value.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## Technologies Used
+- Flutter
+- qr_flutter package for generating QR codes
+- screen_brightness package for controlling screen brightness
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## How to Use
+1. Clone the repository to your local machine.
+2. Ensure you have Flutter installed and set up on your machine.
+3. Open the project in your preferred code editor.
+4. Run `flutter pub get` to install dependencies.
+5. Run the app using `flutter run`.
+
+## Screenshots
+ 
+
+## Contributing
+Contributions are welcome! Please feel free to fork the repository and submit pull requests to contribute to this project.
+ 
+
+ ------------------------------------------------------------------
+
+
+# QR Kod Sayfası Parlaklık Örneği
+
+Bu Flutter uygulaması, bir QR kodu oluşturmayı ve navigasyon olaylarına bağlı olarak ekran parlaklığını kontrol etmeyi göstermektedir.
+
+## Açıklama
+
+Uygulama, bir QR kodunun görüntülendiği tek bir sayfadan (`RouteAwarePage`) oluşur. QR kodu, bir URL bağlantısıyla birlikte görüntülenir. QR kodu, belirli bir hedefe yönlendiren kısaltılmış bir URL'ye gider.Bu sayfa açılır açılmaz ekran parlaklığı otomatik yükselir bu şekilde turnike geçişlerinde parlaklıktan dolayı kodun okunmaması olayı çözülmüş olur.
+
+`RouteAwarePage`'e gidildiğinde ve oradan ayrıldığında ekran parlaklığı aşağıdaki gibi ayarlanır:
+- Sayfa itildiğinde, parlaklık %70'e ayarlanır.
+- Sayfadan ayrıldığında, parlaklık varsayılan değerine sıfırlanır.
+
+## Kullanılan Teknolojiler
+- Flutter
+- QR kodları oluşturmak için qr_flutter paketi
+- Ekran parlaklığını kontrol etmek için screen_brightness paketi
+
+## Nasıl Kullanılır
+1. Depoyu yerel makinenize klonlayın.
+2. Flutter'ın makinenizde yüklü ve yapılandırılmış olduğundan emin olun.
+3. Projeyi tercih ettiğiniz kod düzenleyicide açın.
+4. Bağımlılıkları yüklemek için `flutter pub get` komutunu çalıştırın.
+5. Uygulamayı `flutter run` komutunu kullanarak çalıştırın.
+
+## Ekran Görüntüleri
+ 
+
+
+ 
+ 
--- lib/main.dart
+++ lib/main.dart
@@ -1,404 +1,105 @@
-// import 'dart:io';
+// ignore_for_file: library_private_types_in_public_api
 
-// import 'package:flutter/material.dart';
-// import 'package:screen_brightness/screen_brightness.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:screen_brightness/screen_brightness.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   static final RouteObserver<Route> routeObserver = RouteObserver<Route>();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static final RouteObserver<Route> routeObserver = RouteObserver<Route>();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const RouteAwarePage(),
+      navigatorObservers: [
+        routeObserver,
+      ],
+    );
+  }
+}
 
-//   const MyApp({Key? key}) : super(key: key);
+class RouteAwarePage extends StatefulWidget {
+  static const routeName = '/routeAware';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: const HomePage(),
-//       onGenerateRoute: (settings) {
-//         late final Widget page;
-//         switch (settings.name) {
-//           case HomePage.routeName:
-//             page = const HomePage();
-//             break;
+  const RouteAwarePage({Key? key}) : super(key: key);
 
-//           case ControllerPage.routeName:
-//             page = const ControllerPage();
-//             break;
+  @override
+  _RouteAwarePageState createState() => _RouteAwarePageState();
+}
 
-//           case RouteAwarePage.routeName:
-//             page = const RouteAwarePage();
-//             break;
+class _RouteAwarePageState extends State<RouteAwarePage> with RouteAware {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    MyApp.routeObserver.subscribe(this, ModalRoute.of(context)!);
+  }
 
-//           case BlankPage.routeName:
-//             page = const BlankPage();
-//             break;
+  @override
+  void dispose() {
+    MyApp.routeObserver.unsubscribe(this);
+    super.dispose();
+  }
 
-//           case SettingPage.routeName:
-//             page = const SettingPage();
-//             break;
+  @override
+  void didPush() {
+    super.didPush();
+    ScreenBrightness().setScreenBrightness(0.7);
+  }
 
-//           default:
-//             throw UnimplementedError('page name not found');
-//         }
+  @override
+  void didPushNext() {
+    super.didPushNext();
+    ScreenBrightness().resetScreenBrightness();
+  }
 
-//         return MaterialPageRoute(
-//           builder: (context) => page,
-//           settings: settings,
-//         );
-//       },
-//       navigatorObservers: [
-//         routeObserver,
-//       ],
-//     );
-//   }
-// }
+  @override
+  void didPop() {
+    super.didPop();
+    ScreenBrightness().resetScreenBrightness();
+  }
 
-// class HomePage extends StatelessWidget {
-//   static const routeName = '/home';
+  @override
+  void didPopNext() {
+    super.didPopNext();
+    ScreenBrightness().setScreenBrightness(0.7);
+  }
 
-//   const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    String url = 'https://bitly.ws/3beFZ'; //Where will the qr code be directed?
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Screen brightness example'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             FutureBuilder<double>(
-//               future: ScreenBrightness().current,
-//               builder: (context, snapshot) {
-//                 double currentBrightness = 0;
-//                 if (snapshot.hasData) {
-//                   currentBrightness = snapshot.data!;
-//                 }
-
-//                 return StreamBuilder<double>(
-//                   stream: ScreenBrightness().onCurrentBrightnessChanged,
-//                   builder: (context, snapshot) {
-//                     double changedBrightness = currentBrightness;
-//                     if (snapshot.hasData) {
-//                       changedBrightness = snapshot.data!;
-//                     }
-
-//                     return Text('current brightness $changedBrightness');
-//                   },
-//                 );
-//               },
-//             ),
-//             ElevatedButton(
-//               onPressed: () =>
-//                   Navigator.of(context).pushNamed(ControllerPage.routeName),
-//               child: const Text('Controller example page'),
-//             ),
-//             ElevatedButton(
-//               onPressed: () =>
-//                   Navigator.of(context).pushNamed(RouteAwarePage.routeName),
-//               child: const Text('Route aware example page'),
-//             ),
-//             ElevatedButton(
-//               onPressed: () =>
-//                   Navigator.of(context).pushNamed(SettingPage.routeName),
-//               child: const Text('Setting page'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class ControllerPage extends StatefulWidget {
-//   static const routeName = '/controller';
-
-//   const ControllerPage({Key? key}) : super(key: key);
-
-//   @override
-//   State<ControllerPage> createState() => _ControllerPageState();
-// }
-
-// class _ControllerPageState extends State<ControllerPage> {
-//   Future<void> setBrightness(double brightness) async {
-//     try {
-//       await ScreenBrightness().setScreenBrightness(brightness);
-//     } catch (e) {
-//       debugPrint(e.toString());
-//       throw 'Failed to set brightness';
-//     }
-//   }
-
-//   Future<void> resetBrightness() async {
-//     try {
-//       await ScreenBrightness().resetScreenBrightness();
-//     } catch (e) {
-//       debugPrint(e.toString());
-//       throw 'Failed to reset brightness';
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Controller'),
-//       ),
-//       body: Center(
-//         child: FutureBuilder<double>(
-//           future: ScreenBrightness().current,
-//           builder: (context, snapshot) {
-//             double currentBrightness = 0;
-//             if (snapshot.hasData) {
-//               currentBrightness = snapshot.data!;
-//             }
-
-//             return StreamBuilder<double>(
-//               stream: ScreenBrightness().onCurrentBrightnessChanged,
-//               builder: (context, snapshot) {
-//                 double changedBrightness = currentBrightness;
-//                 if (snapshot.hasData) {
-//                   changedBrightness = snapshot.data!;
-//                 }
-
-//                 return Column(
-//                   mainAxisSize: MainAxisSize.min,
-//                   children: [
-//                     FutureBuilder<bool>(
-//                       future: ScreenBrightness().hasChanged,
-//                       builder: (context, snapshot) {
-//                         return Text(
-//                             'Brightness has changed via plugin: ${snapshot.data}');
-//                       },
-//                     ),
-//                     Text('Current brightness: $changedBrightness'),
-//                     Slider.adaptive(
-//                       value: changedBrightness,
-//                       onChanged: (value) {
-//                         setBrightness(value);
-//                       },
-//                     ),
-//                     ElevatedButton(
-//                       onPressed: () {
-//                         resetBrightness();
-//                       },
-//                       child: const Text('reset brightness'),
-//                     ),
-//                   ],
-//                 );
-//               },
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class RouteAwarePage extends StatefulWidget {
-//   static const routeName = '/routeAware';
-
-//   const RouteAwarePage({Key? key}) : super(key: key);
-
-//   @override
-//   _RouteAwarePageState createState() => _RouteAwarePageState();
-// }
-
-// class _RouteAwarePageState extends State<RouteAwarePage> with RouteAware {
-//   @override
-//   void didChangeDependencies() {
-//     super.didChangeDependencies();
-//     MyApp.routeObserver.subscribe(this, ModalRoute.of(context)!);
-//   }
-
-//   @override
-//   void dispose() {
-//     MyApp.routeObserver.unsubscribe(this);
-//     super.dispose();
-//   }
-
-//   @override
-//   void didPush() {
-//     super.didPush();
-//     ScreenBrightness().setScreenBrightness(0.7);
-//   }
-
-//   @override
-//   void didPushNext() {
-//     super.didPushNext();
-//     ScreenBrightness().resetScreenBrightness();
-//   }
-
-//   @override
-//   void didPop() {
-//     super.didPop();
-//     ScreenBrightness().resetScreenBrightness();
-//   }
-
-//   @override
-//   void didPopNext() {
-//     super.didPopNext();
-//     ScreenBrightness().setScreenBrightness(0.7);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Route aware'),
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () => Navigator.of(context).pushNamed(BlankPage.routeName),
-//           child: const Text('Next page'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class BlankPage extends StatelessWidget {
-//   static const routeName = '/blankPage';
-
-//   const BlankPage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Blank'),
-//       ),
-//     );
-//   }
-// }
-
-// class SettingPage extends StatefulWidget {
-//   static const routeName = '/setting';
-
-//   const SettingPage({Key? key}) : super(key: key);
-
-//   @override
-//   _SettingPageState createState() => _SettingPageState();
-// }
-
-// class _SettingPageState extends State<SettingPage> {
-//   bool isAutoReset = true;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     getAutoResetSetting();
-//   }
-
-//   Future<void> getAutoResetSetting() async {
-//     final _isAutoReset = await ScreenBrightness().isAutoReset;
-//     setState(() {
-//       isAutoReset = _isAutoReset;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Setting'),
-//       ),
-//       body: ListView(
-//         children: [
-//           ListTile(
-//             title: const Text('Auto Reset (iOS only)'),
-//             trailing: Switch(
-//               value: isAutoReset,
-//               onChanged:
-//                   Platform.isIOS || Platform.isWindows || Platform.isMacOS
-//                       ? (value) async {
-//                           await ScreenBrightness().setAutoReset(value);
-//                           await getAutoResetSetting();
-//                         }
-//                       : null,
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// import 'package:flutter/material.dart';
-// import 'package:photo_view/photo_view.dart';
-// import 'package:photo_view/photo_view_gallery.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: MyGallery(),
-//     );
-//   }
-// }
-
-// class MyGallery extends StatefulWidget {
-//   @override
-//   _MyGalleryState createState() => _MyGalleryState();
-// }
-
-// class _MyGalleryState extends State<MyGallery> {
-//   final List<String> imageUrls = [
-//     "https://via.placeholder.com/300",
-//     "https://via.placeholder.com/300",
-//     "https://via.placeholder.com/300",
-//     // ... diğer resim URL'leri
-//   ];
-
-//   int currentIndex = 0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Resim Galerisi'),
-//       ),
-//       body: PhotoViewGallery.builder(
-//         itemCount: imageUrls.length,
-//         builder: (context, index) {
-//           return PhotoViewGalleryPageOptions(
-//             imageProvider: NetworkImage(imageUrls[index]),
-//             minScale: PhotoViewComputedScale.contained,
-//             maxScale: PhotoViewComputedScale.covered * 2,
-//           );
-//         },
-//         backgroundDecoration: BoxDecoration(
-//           color: Colors.black,
-//         ),
-//         pageController: PageController(),
-//         onPageChanged: (index) {
-//           setState(() {
-//             currentIndex = index;
-//           });
-//         },
-//       ),
-//       bottomNavigationBar: BottomAppBar(
-//         color: Colors.transparent,
-//         child: Container(
-//           padding: EdgeInsets.symmetric(vertical: 10.0),
-//           child: Text(
-//             'Resim ${currentIndex + 1} / ${imageUrls.length}',
-//             textAlign: TextAlign.center,
-//             style: TextStyle(
-//               color: Colors.white,
-//               fontSize: 16.0,
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('QRCode Brightness Example'),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(96, 193, 193, 193)),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 70, bottom: 30),
+            child: Text('Have your QR code scanned by the turnstile'),
+          ),
+          Center(
+            child: QrImageView(
+              data: url,
+              size: 320,
+              gapless: false,
+              errorStateBuilder: (cxt, err) {
+                return const Center(
+                  child: Text(
+                    'Sorry something went wrong',
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
